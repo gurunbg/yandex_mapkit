2e89c39ee4c31ef95c38d69a34269b8974caf62d
gurunbg <gurun.bg@gmail.com>
2023-02-13 21:37:30 +0500

    Fix null pointer exception

--- lib/src/types/search/search_options.dart
+++ lib/src/types/search/search_options.dart
@@ -37,7 +37,7 @@ class SearchOptions extends Equatable {
       'geometry': geometry,
       'disableSpellingCorrection': disableSpellingCorrection,
       'resultPageSize': resultPageSize,
-      'userPosition': userPosition.toJson(),
+      'userPosition': userPosition?.toJson(),
       'origin': origin
     };
   }
